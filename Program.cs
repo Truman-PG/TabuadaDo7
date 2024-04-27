13cdff5f4c874edcab1516d4132b9fc90592fbe6
Aluno <Aluno@DESKTOP-DMGHJ3C>
2024-04-27 12:20:13 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Tabuadas");
+int numero = 0;
+int tabuada = 0;
+string resposta;
+
+do
+{
+    Console.WriteLine("Qual a tabuada você deseja?");
+    tabuada = int.Parse(Console.ReadLine());
+
+    while (numero <= 10)
+    {
+        Console.WriteLine($"{tabuada} x {numero} = {numero * tabuada}");
+        numero++;
+    }
+    numero = 0;
+
+    Console.WriteLine("Deseja gerar outra tabuada?");
+    resposta = Console.ReadLine();
+    Console.Clear();
+} while (resposta == "s");
